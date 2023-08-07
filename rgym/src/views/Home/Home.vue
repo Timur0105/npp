772a86e7987816244368d943628884939d32ab9d
Timur0 <timurjoldasbaev02@gmail.com>
2023-08-07 23:30:24 +0300

    second commit

--- rgym/src/views/Home/Home.vue
+++ rgym/src/views/Home/Home.vue
@@ -59,14 +59,72 @@
       <div :class="styles.home_popular_plan">
         <div :class="styles.home_popular_plan_text1">
           <p :class="styles.home_popular_plan_text">
-            The ultimate online online personal training plans
+            The ultimate online personal training plans
           </p>
         </div>
         <div :class="styles.home_popular_plan_sidetext">
+          <section :class="styles.home_popular_plan_sidetext_line"></section>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dui neque sa
           gittis magna erat sagittis morbi. Id libero. Lorem ipsum dolor sit amet,
           consectetur adipiscing elit. Aliquam dui neque sa gittis magna erat sagittis
           morbi. Id libero.
+        </div>
+      </div>
+    </div>
+    <div :class="styles.home_training">
+      <div :class="styles.home_training_intro">
+        <img
+          src="@/assets/images/train01.png"
+          alt="Training"
+          :class="styles.home_training_intro_img"
+        />
+        <div :class="styles.home_training_intro_text">
+          <p :class="styles.home_training_intro_text_title">Personal Training</p>
+          <p :class="styles.home_training_intro_text_subtitle">Gym time optimized</p>
+          <p :class="styles.home_training_intro_text_subtext">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dui neque sa
+            gittis magna erat sagittis morbi. Id libero. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Aliquam dui neque sa gittis magna erat sagittis
+            morbi. Id libero.
+          </p>
+        </div>
+      </div>
+      <div :class="styles.home_training_fullimages">
+        <img
+          src="@/assets/images/train02.png"
+          alt="Training02"
+          :class="styles.home_training_fullimages_n2"
+        />
+        <img
+          src="@/assets/images/train03.png"
+          alt="Training03"
+          :class="styles.home_training_fullimages_n3"
+        />
+        <img
+          src="@/assets/images/train04.png"
+          alt="Training04"
+          :class="styles.home_training_fullimages_n4"
+        />
+        <img
+          src="@/assets/images/train05.png"
+          alt="Training05"
+          :class="styles.home_training_fullimages_n5"
+        />
+        <img
+          src="@/assets/images/train06.png"
+          alt="Training06"
+          :class="styles.home_training_fullimages_n6"
+        />
+        <div :class="styles.home_training_fullimages_textbox">
+          <img
+            src="@/assets/images/welcome.png"
+            alt="Welcom"
+            :class="styles.home_training_fullimages_textbox_title"
+          />
+          <p :class="styles.home_training_fullimages_textbox_text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dui neque sa
+            gittis magna erat sagittis morbi. Id libero.
+          </p>
         </div>
       </div>
     </div>
